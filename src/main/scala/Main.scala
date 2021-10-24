9b1d6ed70c02dbab80c0a176e9e9a83b34843100
Weiqifan1 <cmlykke@hotmail.com>
2021-10-24 20:44:13 +0200

    2021-10-24_2043: junda,tzaiAndcedictMaps

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,64 @@
 import allheisigcharacters.{allChars, allCharsSaveToFiles}
 import personalStoriesParseInput.{createListOfStoryArrays, linesToArr, listOfPrivateStories, saveListOfStoriesToFileTradHanzi, startOfCSVEntry}
 
+import scala.collection.immutable.HashMap
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object Main {
 
-  /*
-  var i=0
-var length=0
-val data=Source.fromFile(file)
-for (line <- data.getLines) {
-  val cols = line.split(",").map(_.trim)
-  length = cols.length
-  while(i<length){
-    //println(cols(i))
-    i=i+1
+  def createJundaAndTzai(filePath: String): Map[String, Int] = {
+    val source = scala.io.Source.fromFile(filePath)
+    val data: Array[String] = source.mkString("").split("")
+
+    var hashmap = new mutable.HashMap[String, Int]()
+    for(x: Int <- 0 to (data.length-1)){
+      val each: String = data(x)
+      hashmap.put(each, x)
+    }
+    val finalObject: Map[String, Int] = Map() ++ hashmap;
+    return finalObject;
   }
-  i=0
-}
-  */
+
+  def createCedictMap(getTraditional: Boolean): Map[String, String] = {
+    val source = scala.io.Source.fromFile("heisigStories/dictionariesAndLists/cedict_ts.txt")
+    val data: Array[Array[String]] = source.getLines.map(_.split("\\s+")).toArray
+    source.close
+    var dyadArray: Array[(String, String)] = null
+    if (getTraditional) {
+      dyadArray = data.map(eachArr => {
+        val firstitem: String = eachArr(0)
+        val allChars: String = eachArr.mkString(" ")
+        (firstitem, allChars)
+      })
+    }else {
+      dyadArray = data.map(eachArr => {
+        val firstitem: String = eachArr(1)
+        val allChars: String = eachArr.mkString(" ")
+        (firstitem, allChars)
+      })
+    }
+    var hashmap = new mutable.HashMap[String, String]()
+    for(x: (String, String) <- dyadArray ){
+      hashmap.put(x._1, x._2)
+    }
+    val finalObject: Map[String, String] = Map() ++ hashmap;
+    return finalObject;
+  }
 
   def main(args: Array[String]): Unit = {
     println("hello chr")
     //save person stories (only traditional)
     //create a dictionary hashmap
+
+    val junda: Map[String, Int] = createJundaAndTzai("heisigStories/dictionariesAndLists/testJunda.txt")
+    val tzai: Map[String, Int] = createJundaAndTzai("heisigStories/dictionariesAndLists/testTzai.txt")
+
+    val cedictTradDictionary: Map[String, String] = createCedictMap(true)
+    val cedictSimpDictionary: Map[String, String] = createCedictMap(false)
+
+
 
     println("dictionary hashmap created")
     val myStories: List[Array[String]] = createListOfStoryArrays()
@@ -33,9 +67,6 @@ for (line <- data.getLines) {
 
     //save default character list
     //allCharsSaveToFiles()
-
-
-
   }
 
 
