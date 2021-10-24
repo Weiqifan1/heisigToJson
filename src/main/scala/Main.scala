9128589af00d35f366647128a6f425743b81c8ce
Weiqifan1 <cmlykke@hotmail.com>
2021-10-24 22:23:36 +0200

    2021-10-24_2223: dictionaryKodeVirker

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -23,7 +23,7 @@ object Main {
 
   def createCedictMap(getTraditional: Boolean): Map[String, String] = {
     val source = scala.io.Source.fromFile("heisigStories/dictionariesAndLists/cedict_ts.txt")
-    val data: Array[Array[String]] = source.getLines.map(_.split("\\s+")).toArray
+    var data: Array[Array[String]] = source.getLines.map(_.split("\\s+")).toArray
     source.close
     var dyadArray: Array[(String, String)] = null
     if (getTraditional) {
@@ -39,8 +39,42 @@ object Main {
         (firstitem, allChars)
       })
     }
+    dyadArray.sortBy(_._1)
+    //****
+
+    var greatNestedBuffer: ListBuffer[ListBuffer[(String, String)]] = new ListBuffer[ListBuffer[(String, String)]]
+    var tempListBuffer: ListBuffer[ (String, String)] = new ListBuffer[(String, String)]()
+    for (itemNumber <- Range.inclusive(1, dyadArray.length-2)) {
+      val previousItem: (String, String) = dyadArray(itemNumber-1)
+      val currentItem: (String, String) = dyadArray(itemNumber)
+      if (currentItem._1.equals("的")) {
+        val test = "hello"
+      }
+
+      val nextItem: (String, String) = dyadArray(itemNumber+1)
+      if (previousItem._1.equals(currentItem._1) && nextItem._1.equals(currentItem._1)) { // previous and current and next are the same
+        tempListBuffer.addOne(currentItem)
+      }else if (previousItem._1.equals(currentItem._1) && !nextItem._1.equals(currentItem._1)) { //previous and current are the same, but the next is a new one
+        tempListBuffer.addOne(currentItem)
+        greatNestedBuffer.addOne(tempListBuffer)
+        tempListBuffer = new ListBuffer[(String, String)]()
+      }else if (!previousItem._1.equals(currentItem._1) && nextItem._1.equals(currentItem._1)) { //previous was different, but the next is the same
+        tempListBuffer = new ListBuffer[(String, String)]()
+        tempListBuffer.addOne(currentItem)
+      }else {                                                                       //previous was different and next is different
+        tempListBuffer = new ListBuffer[(String, String)]()
+        tempListBuffer.addOne(currentItem)
+        greatNestedBuffer.addOne(tempListBuffer)
+        tempListBuffer = new ListBuffer[(String, String)]()
+      }
+    }
+    val reducedNestedList: List[(String, String)] = greatNestedBuffer.map(eachNested => {
+      (eachNested(0)._1, eachNested.map(_._2).toList.mkString("\n"))
+    }).toList
+
+
     var hashmap = new mutable.HashMap[String, String]()
-    for(x: (String, String) <- dyadArray ){
+    for(x: (String, String) <- reducedNestedList ){
       hashmap.put(x._1, x._2)
     }
     val finalObject: Map[String, String] = Map() ++ hashmap;
